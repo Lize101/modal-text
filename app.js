8e0adede83a81963ff656b62c4d30aa8ef22449b
Lizdigital <lizetteclaireh@gmail.com>
2024-04-27 20:24:57 +0100

    Add close modal function and click event listener

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // Variables
-
 const btn = document.getElementById('btn');
 const container = document.getElementById('modal-container');
 const close = document.getElementById('close-modal');
 
+// Function to view the modal
 function openModal () {
     container.style.display = 'block';
 }
 
+// Click event listener to open the modal when the button is clicked
 btn.addEventListener('click', openModal);
+
+
+// Function to hide the modal
+function closeModal () {
+    container.style.display = 'none';
+}
+
+// Click event listener to close the modal when the 'X' is clicked
+close.addEventListener('click', closeModal);
