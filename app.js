d942546afbef4e365e982d84efb88e988610ace5
Lizdigital <lizetteclaireh@gmail.com>
2024-04-27 20:19:39 +0100

    Add open modal function and click event listener

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const btn = document.getElementById('btn');
 const container = document.getElementById('modal-container');
 const close = document.getElementById('close-modal');
 
+function openModal () {
+    container.style.display = 'block';
+}
+
+btn.addEventListener('click', openModal);
