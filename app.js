3ef83203c08cef9b8e0edf43a477feec2da8034b
Lizdigital <lizetteclaireh@gmail.com>
2024-04-29 21:34:23 +0100

    Add event listener - hide modal when the body is clicked

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const btn = document.getElementById('btn');
 const container = document.getElementById('modal-container');
 const close = document.getElementById('close-modal');
+const body = document.getElementById('body');
 
 // Function to view the modal
 function openModal () {
@@ -19,3 +20,16 @@ function closeModal () {
 
 // Click event listener to close the modal when the 'X' is clicked
 close.addEventListener('click', closeModal);
+
+
+
+window.addEventListener('click', function(e) {
+    //Test, in the console, to see whether body matches e.target
+    console.log(body); 
+    //Test, in the console, to see whether e.target matches body
+    console.log(e.target);
+    // If statement that closes the modal if the user clicks anywhere in the body
+    if (e.target === body) {
+        container.style.display = 'none';
+    }
+})
